f5776e1c8672a757f45a1c424f922479a56a6c9f
Martin <martin_cho@pegatroncorp.com>
2023-10-06 10:28:29 +0800

    add pipfile and main.py to locally run html

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse 
+import multiprocessing
+import os
+
+multiprocessing.freeze_support()
+app = FastAPI()
+
+# 指定静态文件夹的路径
+path = os.getcwd()
+print(path)
+app.mount("/js", StaticFiles(directory="js"), name="js")
+app.mount("/css", StaticFiles(directory="css"), name="css")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/attachment", StaticFiles(directory="attachment"), name="attachment")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("index.html", "r") as file:
+        html_content = file.read()
+    return html_content
+
+# Import API routers
+# from api import router as api_router
+
+# app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=9999)
